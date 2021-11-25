6af4a3dc5e4a9b8dbd370eec4cf7a8c694a4372a
Juliano Curti <juliano.curti@tuntscorp.com>
2021-11-25 20:51:35 -0300

    initial commit

---
+++ src/test/java/com/cissbank/basiccissbankapi/BasicCissBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cissbank.basiccissbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCissBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
