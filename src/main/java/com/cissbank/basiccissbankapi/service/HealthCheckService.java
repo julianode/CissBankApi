cb3da8c3ff812c9af842e618ef114ee48edb23a0
Juliano Curti <juliano.curti@tuntscorp.com>
2021-11-25 21:40:55 -0300

    Fix healthCheck message

--- src/main/java/com/cissbank/basiccissbankapi/service/HealthCheckService.java
+++ src/main/java/com/cissbank/basiccissbankapi/service/HealthCheckService.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.time.LocalTime;
 import java.util.concurrent.atomic.AtomicLong;
@@ -14,7 +15,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class HealthCheckService {
 
     private static final Logger log = LoggerFactory.getLogger("HealthCheckService");
-    private static final String template = "The System started at %s, and is running for %s.";
+    private static final String template = "CiSS Bank Basic API is running. [Start time: %s]";
     private final AtomicLong counter = new AtomicLong();
     private final long startTime = System.currentTimeMillis();
 
@@ -22,10 +23,9 @@ public class HealthCheckService {
     public HealthCheck healthCheck() {
 
         Timestamp startTime = new Timestamp(this.startTime);
-        long startTimeInSeconds = this.startTime/1000;
-        LocalTime runningTime = LocalTime.now().minusSeconds(startTimeInSeconds);
+        long millisecondsTimeDifference = System.currentTimeMillis() - this.startTime;
         log.info("System healthCheck requested. [count: {}]", counter.incrementAndGet());
 
-        return new HealthCheck(counter.get(), String.format(template, startTime, runningTime));
+        return new HealthCheck(counter.get(), String.format(template, startTime));
     }
 }
